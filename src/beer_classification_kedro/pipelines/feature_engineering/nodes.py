82b10901516b06623bde11e98b0d94741574c3eb
Cristian Sanchez Pineda <christian.sanchez@kinesso.com>
2022-10-18 18:33:31 -0500

    Completed the feature engineering pipeline

--- src/beer_classification_kedro/pipelines/feature_engineering/nodes.py
+++ src/beer_classification_kedro/pipelines/feature_engineering/nodes.py
@@ -4,6 +4,27 @@ generated using Kedro 0.18.3
 """
 import pandas as pd 
 from sklearn.utils import resample
+from sklearn.preprocessing import LabelEncoder
+
+
+
+def encode_label(data: pd.DataFrame) -> pd.DataFrame:
+    '''
+    This function allows to encode the label
+    '''
+    label_encoder = LabelEncoder()
+    data['Style'] = label_encoder.fit_transform(
+        data['Style'])
+    return data
+
+
+def create_dummies_variables(data: pd.DataFrame) -> pd.DataFrame:
+    '''
+    This function allows to create the dummies variables for the categoric columns
+    '''
+    categoric_keys = ['BrewMethod', 'SugarScale', 'Style']
+    return pd.get_dummies(data, columns=categoric_keys[:2])
+
 
 def resample_classes(data: pd.DataFrame) -> pd.DataFrame:
     '''
@@ -14,14 +35,15 @@ def resample_classes(data: pd.DataFrame) -> pd.DataFrame:
     styles_unbalanced = [k for k, v in count_df.items() if v <= 100]
 
     df_sampled = pd.DataFrame()
+
     for j in styles_unbalanced:
 
         df_minority_j = data[data.Style == j]
-        df_minority_upsampled = resample(df_minority_j,
-                                         replace=True,
-                                         n_samples=400,
-                                         stratify=df_minority_j,
-                                         random_state=123)
-        df_sampled = pd.concat([df_sampled, df_minority_upsampled])
-
-    return df_sampled
+        if df_minority_j.shape[0] != 0:
+            df_minority_upsampled = resample(df_minority_j,
+                                             replace=True,
+                                             n_samples=400,
+                                             stratify=df_minority_j,
+                                             random_state=123)
+            df_sampled = pd.concat([df_sampled, df_minority_upsampled])
+    return pd.concat([data, df_sampled])
--- src/beer_classification_kedro/pipelines/feature_engineering/pipeline.py
+++ src/beer_classification_kedro/pipelines/feature_engineering/pipeline.py
@@ -3,14 +3,22 @@ This is a boilerplate pipeline 'feature_engineering'
 generated using Kedro 0.18.3
 """
 
-from beer_classification_kedro.pipelines.feature_engineering.nodes import resample_classes
+from beer_classification_kedro.pipelines.feature_engineering.nodes import create_dummies_variables, encode_label, resample_classes
 from kedro.pipeline import Pipeline, node, pipeline
 
-
+     
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([
-        node(func=resample_classes,
+        node(func=create_dummies_variables,
              inputs="data_clean",
+             outputs="data_dummies",
+             name="dummies_categoric"),
+        node(func=encode_label,
+             inputs="data_dummies",
+             outputs="data_labeled",
+             name="encode_classes"),
+        node(func=resample_classes,
+             inputs="data_labeled",
              outputs="data_balanced",
              name="balance_classes")
     ])
